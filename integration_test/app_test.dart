d019212e87923e02545479eb9f1dc6b5dfa74420
hrishiksh <hrishi1pathak@gmail.com>
2022-08-21 09:41:37 +0530

    first commit

---
+++ integration_test/app_test.dart
@@ -0,0 +1,26 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+
+import 'package:flutter_firebase_emulator/main.dart' as app;
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  group("End-to-end-testing", () {
+    testWidgets("Tapping on floating action button and varify counter",
+        (tester) async {
+      app.main();
+      await tester.pumpAndSettle();
+
+      expect(find.text("0"), findsOneWidget);
+
+      final Finder fab = find.byTooltip("Increment");
+
+      await tester.tap(fab);
+
+      await tester.pumpAndSettle();
+
+      expect(find.text("1"), findsOneWidget);
+    });
+  });
+}
---
+++ lib/main.dart
@@ -0,0 +1,93 @@
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+import "package:firebase_core/firebase_core.dart";
+import 'firebase_options.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late FirebaseFirestore firestore;
+  int _counter = 0;
+  @override
+  void initState() {
+    firestore = FirebaseFirestore.instance;
+    firestore.settings =
+        const Settings(persistenceEnabled: false, sslEnabled: false);
+    firestore.useFirestoreEmulator('localhost', 8080);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            StreamBuilder<DocumentSnapshot<Map<String, dynamic>>>(
+                stream:
+                    firestore.collection("counter").doc("count").snapshots(),
+                builder: (context, snapshot) {
+                  if (snapshot.hasError) {
+                    return Text(
+                      "Somthing is wrong",
+                      style: Theme.of(context).textTheme.headline4,
+                    );
+                  }
+                  return Text(
+                    snapshot.data?.data()?["count"].toString() ?? "0",
+                    style: Theme.of(context).textTheme.headline4,
+                  );
+                }),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _counter++;
+          firestore.collection("counter").doc("count").set({"count": _counter});
+        },
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
